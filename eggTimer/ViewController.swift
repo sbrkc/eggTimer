dd19adbe060dd6a40c43801a3fb3248d955bd035
Sabri <gitarist_sabri>
2023-01-11 09:29:44 +0300

    UI okay.

--- eggTimer/ViewController.swift
+++ eggTimer/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func hardnessSelected(_ sender: Any) {
+        let hardnessSelected = sender.currentTitle
+        
+        if(hardnessSelected)
+        
+        }
+    
 }
 
